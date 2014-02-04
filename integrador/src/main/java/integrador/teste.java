608455b1bd9d31e0b2dd80ef185dedd2056b1f17
Mariana Hiromi Sonoda <mariana.sonoda@dls107.lab.dextra.com.br>
2014-02-04 13:30:51 -0200

    sem o wlsd

--- integrador/src/main/java/integrador/teste.java
+++ integrador/src/main/java/integrador/teste.java
@@ -17,21 +17,23 @@ public static void main(String[] args) {
 	NotaFiscalResource n = new NotaFiscalResource();
 	NotaFiscalResourcePortType no = n.getNotaFiscalResourcePort();
 	List<NotaFiscal> listaNota = no.list();
+	System.out.println("listagem do numero das notas fiscais:");
 	for (NotaFiscal notaFiscal : listaNota) {
 		System.out.println(notaFiscal.getNumero());
 	}
+	
 	ClienteResource c = new ClienteResource();
 	ClienteResourcePortType cl = c.getClienteResourcePort();
-	
 	List<Cliente> listaCliente = cl.list();
+	System.out.println("listagem do nome dos clientes:");
 	for (Cliente cliente : listaCliente) {
 		System.out.println(cliente.getNome());
 	}
 	
 	ProdutoResource p = new ProdutoResource();
 	ProdutoResourcePortType pr = p.getProdutoResourcePort();
-	
 	List<Produto> listaProduto = pr.list();
+	System.out.println("listagem do numero dos produtos:");
 	for (Produto produto : listaProduto) {
 		System.out.println(produto.getNome());
 	}
--- integrador/src/main/soa32.faturamento.resource/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://resource.faturamento.soa32/")
-package soa32.faturamento.resource;
