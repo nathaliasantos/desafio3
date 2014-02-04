4a8d902a52e47bca86cca53307986177f427534f
Mariana Hiromi Sonoda <mariana.sonoda@dls107.lab.dextra.com.br>
2014-02-04 10:50:05 -0200

    cxf funcionando

---
+++ integrador/src/main/java/ObjectFactory.java
@@ -0,0 +1,279 @@
+
+package soa32.faturamento.resource;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the soa32.faturamento.resource package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+    private final static QName _UpdateResponse_QNAME = new QName("http://resource.faturamento.soa32/", "updateResponse");
+    private final static QName _DeleteAll_QNAME = new QName("http://resource.faturamento.soa32/", "deleteAll");
+    private final static QName _Cliente_QNAME = new QName("http://resource.faturamento.soa32/", "cliente");
+    private final static QName _DeleteResponse_QNAME = new QName("http://resource.faturamento.soa32/", "deleteResponse");
+    private final static QName _CreateResponse_QNAME = new QName("http://resource.faturamento.soa32/", "createResponse");
+    private final static QName _Delete_QNAME = new QName("http://resource.faturamento.soa32/", "delete");
+    private final static QName _ListResponse_QNAME = new QName("http://resource.faturamento.soa32/", "listResponse");
+    private final static QName _DeleteAllResponse_QNAME = new QName("http://resource.faturamento.soa32/", "deleteAllResponse");
+    private final static QName _Get_QNAME = new QName("http://resource.faturamento.soa32/", "get");
+    private final static QName _Update_QNAME = new QName("http://resource.faturamento.soa32/", "update");
+    private final static QName _Create_QNAME = new QName("http://resource.faturamento.soa32/", "create");
+    private final static QName _List_QNAME = new QName("http://resource.faturamento.soa32/", "list");
+    private final static QName _GetResponse_QNAME = new QName("http://resource.faturamento.soa32/", "getResponse");
+    private final static QName _BaseEntity_QNAME = new QName("http://resource.faturamento.soa32/", "baseEntity");
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: soa32.faturamento.resource
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link Update }
+     * 
+     */
+    public Update createUpdate() {
+        return new Update();
+    }
+
+    /**
+     * Create an instance of {@link Get }
+     * 
+     */
+    public Get createGet() {
+        return new Get();
+    }
+
+    /**
+     * Create an instance of {@link List }
+     * 
+     */
+    public List createList() {
+        return new List();
+    }
+
+    /**
+     * Create an instance of {@link GetResponse }
+     * 
+     */
+    public GetResponse createGetResponse() {
+        return new GetResponse();
+    }
+
+    /**
+     * Create an instance of {@link Create }
+     * 
+     */
+    public Create createCreate() {
+        return new Create();
+    }
+
+    /**
+     * Create an instance of {@link DeleteResponse }
+     * 
+     */
+    public DeleteResponse createDeleteResponse() {
+        return new DeleteResponse();
+    }
+
+    /**
+     * Create an instance of {@link DeleteAll }
+     * 
+     */
+    public DeleteAll createDeleteAll() {
+        return new DeleteAll();
+    }
+
+    /**
+     * Create an instance of {@link Cliente }
+     * 
+     */
+    public Cliente createCliente() {
+        return new Cliente();
+    }
+
+    /**
+     * Create an instance of {@link UpdateResponse }
+     * 
+     */
+    public UpdateResponse createUpdateResponse() {
+        return new UpdateResponse();
+    }
+
+    /**
+     * Create an instance of {@link CreateResponse }
+     * 
+     */
+    public CreateResponse createCreateResponse() {
+        return new CreateResponse();
+    }
+
+    /**
+     * Create an instance of {@link Delete }
+     * 
+     */
+    public Delete createDelete() {
+        return new Delete();
+    }
+
+    /**
+     * Create an instance of {@link DeleteAllResponse }
+     * 
+     */
+    public DeleteAllResponse createDeleteAllResponse() {
+        return new DeleteAllResponse();
+    }
+
+    /**
+     * Create an instance of {@link ListResponse }
+     * 
+     */
+    public ListResponse createListResponse() {
+        return new ListResponse();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link UpdateResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://resource.faturamento.soa32/", name = "updateResponse")
+    public JAXBElement<UpdateResponse> createUpdateResponse(UpdateResponse value) {
+        return new JAXBElement<UpdateResponse>(_UpdateResponse_QNAME, UpdateResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link DeleteAll }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://resource.faturamento.soa32/", name = "deleteAll")
+    public JAXBElement<DeleteAll> createDeleteAll(DeleteAll value) {
+        return new JAXBElement<DeleteAll>(_DeleteAll_QNAME, DeleteAll.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Cliente }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://resource.faturamento.soa32/", name = "cliente")
+    public JAXBElement<Cliente> createCliente(Cliente value) {
+        return new JAXBElement<Cliente>(_Cliente_QNAME, Cliente.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link DeleteResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://resource.faturamento.soa32/", name = "deleteResponse")
+    public JAXBElement<DeleteResponse> createDeleteResponse(DeleteResponse value) {
+        return new JAXBElement<DeleteResponse>(_DeleteResponse_QNAME, DeleteResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link CreateResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://resource.faturamento.soa32/", name = "createResponse")
+    public JAXBElement<CreateResponse> createCreateResponse(CreateResponse value) {
+        return new JAXBElement<CreateResponse>(_CreateResponse_QNAME, CreateResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Delete }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://resource.faturamento.soa32/", name = "delete")
+    public JAXBElement<Delete> createDelete(Delete value) {
+        return new JAXBElement<Delete>(_Delete_QNAME, Delete.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link ListResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://resource.faturamento.soa32/", name = "listResponse")
+    public JAXBElement<ListResponse> createListResponse(ListResponse value) {
+        return new JAXBElement<ListResponse>(_ListResponse_QNAME, ListResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link DeleteAllResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://resource.faturamento.soa32/", name = "deleteAllResponse")
+    public JAXBElement<DeleteAllResponse> createDeleteAllResponse(DeleteAllResponse value) {
+        return new JAXBElement<DeleteAllResponse>(_DeleteAllResponse_QNAME, DeleteAllResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Get }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://resource.faturamento.soa32/", name = "get")
+    public JAXBElement<Get> createGet(Get value) {
+        return new JAXBElement<Get>(_Get_QNAME, Get.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Update }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://resource.faturamento.soa32/", name = "update")
+    public JAXBElement<Update> createUpdate(Update value) {
+        return new JAXBElement<Update>(_Update_QNAME, Update.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Create }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://resource.faturamento.soa32/", name = "create")
+    public JAXBElement<Create> createCreate(Create value) {
+        return new JAXBElement<Create>(_Create_QNAME, Create.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link List }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://resource.faturamento.soa32/", name = "list")
+    public JAXBElement<List> createList(List value) {
+        return new JAXBElement<List>(_List_QNAME, List.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link GetResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://resource.faturamento.soa32/", name = "getResponse")
+    public JAXBElement<GetResponse> createGetResponse(GetResponse value) {
+        return new JAXBElement<GetResponse>(_GetResponse_QNAME, GetResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link BaseEntity }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://resource.faturamento.soa32/", name = "baseEntity")
+    public JAXBElement<BaseEntity> createBaseEntity(BaseEntity value) {
+        return new JAXBElement<BaseEntity>(_BaseEntity_QNAME, BaseEntity.class, null, value);
+    }
+
+}
---
+++ integrador/src/main/java/UpdateResponse.java
@@ -0,0 +1,62 @@
+
+package soa32.faturamento.resource;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java de updateResponse complex type.
+ * 
+ * <p>O seguinte fragmento do esquema especifica o conteúdo esperado contido dentro desta classe.
+ * 
+ * <pre>
+ * &lt;complexType name="updateResponse">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="return" type="{http://resource.faturamento.soa32/}cliente" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "updateResponse", propOrder = {
+    "_return"
+})
+public class UpdateResponse {
+
+    @XmlElement(name = "return")
+    protected Cliente _return;
+
+    /**
+     * Obtém o valor da propriedade return.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Cliente }
+     *     
+     */
+    public Cliente getReturn() {
+        return _return;
+    }
+
+    /**
+     * Define o valor da propriedade return.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Cliente }
+     *     
+     */
+    public void setReturn(Cliente value) {
+        this._return = value;
+    }
+
+}
