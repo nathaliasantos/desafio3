1be0cbf846f3665f52e93560a819244a34dba4a3
Nathalia Dias dos Santos <nathalia.santos@dls124.lab.dextra.com.br>
2014-02-07 09:58:42 -0200

    a

--- integrador/src/main/java/Utils/ListaUtilsTest.java
+++ integrador/src/main/java/Utils/ListaUtilsTest.java
@@ -1,11 +1,13 @@
 package Utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import junit.framework.TestCase;
 
 import org.junit.Test;
 
 import soa32.resources.cliente.Cliente;
-import soa32.resources.produto.Produto;
 
 public class ListaUtilsTest extends TestCase {
 	@Test
@@ -59,23 +61,4 @@ public class ListaUtilsTest extends TestCase {
 		assertEquals(ListaUtils.listaAdicionarCliente(nova, antiga), result);
 	}
 	
-	
-
-	@Test 
-	public void testeClienteIgual() {
-		Produto p1 = new Produto();
-		p1.setId((long)1);
-		Produto p2 = new Produto();
-		p2.setId((long)1);
-		assertTrue(ListaUtils.igual(p1, p2));
-	}
-	
-	@Test 
-	public void testeProdutoIgual() {
-		Cliente c1 = new Cliente();
-		c1.setId((long)1);
-		Cliente c2 = new Cliente();
-		c2.setId((long)1);
-		assertTrue(ListaUtils.igual(c1, c2));
-	}	
 }
