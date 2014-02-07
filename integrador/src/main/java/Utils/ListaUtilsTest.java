3bc7cb6f604371aca6af91c2cdc77c2848503712
Arissa Tasso Silva <arissa.tasso@dls157.lab.dextra.com.br>
2014-02-07 09:13:56 -0200

    Testes de comparacao iniciados

--- integrador/src/main/java/Utils/ListaUtilsTest.java
+++ integrador/src/main/java/Utils/ListaUtilsTest.java
@@ -1,14 +1,21 @@
 package Utils;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 import junit.framework.TestCase;
 
 import org.junit.Test;
 
-import soa32.resources.cliente.Cliente;
+import soa32.resources.produto.Produto;
 
 public class ListaUtilsTest extends TestCase {
 
+	@Test 
+	public void testeClienteIgual() {
+		Produto prod1 = new Produto();
+		
+	}
+	
+	@Test 
+	public void testeProdutoIgual() {
+		
+	}	
 }
